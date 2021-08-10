78cc0188bdcab2aa4dbda5004a8b0db84fba5e30
Rodrigo Brentano <rodrigobjj24@gmail.com>
2021-08-10 19:06:47 -0300

    Version 1.1

---
+++ index.js
@@ -0,0 +1,23 @@
+function clock() {
+    let data = new Date();
+    let hour = data.getHours();
+    let minute = data.getMinutes();
+    let second = data.getSeconds();
+
+    if (hour < 10) {
+        hour = '0' + hour;
+    }
+    
+    if (minute < 10) {
+        minute = '0' + minute;
+    }
+
+    if (second < 10) {
+        second = '0' + second;
+    }
+
+    let hours = hour + ':' + minute + ":" + second;
+    document.querySelector('.clock').value = hours;
+}
+
+let time = setInterval(clock, 1000);
